6eb816e1f93c48e61ff4efe366a123480b4af442
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-08-14 20:25:56 +0900

    first commit

---
+++ src/main/java/com/example/campusfinder/CampusFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusFinderApplication.class, args);
+    }
+
+}
