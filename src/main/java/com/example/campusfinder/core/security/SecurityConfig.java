2eed5eb4ad3de5ad48d64d585521d9f3023a789e
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-09-07 21:25:24 +0900

    feat: 이메일 암호화 코드 추가

--- src/main/java/com/example/campusfinder/core/security/SecurityConfig.java
+++ src/main/java/com/example/campusfinder/core/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.campusfinder.core.configuration;
+package com.example.campusfinder.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
