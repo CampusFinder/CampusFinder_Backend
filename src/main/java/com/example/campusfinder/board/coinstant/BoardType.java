20f2578eedf55150eb182124a18aec5c3d9b1e60
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-10-12 17:35:18 +0900

    feat: BoardType 클래스 생성

---
+++ src/main/java/com/example/campusfinder/board/coinstant/BoardType.java
@@ -0,0 +1,17 @@
+package com.example.campusfinder.board.coinstant;
+
+/**
+ * packageName    : com.example.campusfinder.board.coinstant
+ * fileName       : BoardType
+ * author         : tlswl
+ * date           : 2024-10-12
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-10-12        tlswl       최초 생성
+ */
+public enum BoardType {
+    REQUEST,
+    HIRE
+}
