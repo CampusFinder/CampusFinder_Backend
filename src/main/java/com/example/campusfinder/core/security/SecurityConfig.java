d143ca4328f545c2edcce0ba8743b953842249bf
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-10-13 23:57:35 +0900

    feat: cors 메서드 추가

--- src/main/java/com/example/campusfinder/core/security/SecurityConfig.java
+++ src/main/java/com/example/campusfinder/core/security/SecurityConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * packageName    : com.example.campusfinder.core.configuration
@@ -31,14 +37,30 @@ public class SecurityConfig {
         this.jwtTokenProvider=jwtTokenProvider;
     }
 
+    //CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // 허용할 출처
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization")); // 노출할 헤더
+        configuration.setAllowCredentials(true); // 자격 증명 사용 여부
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 요청에 대해 CORS 설정
+        return source;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
+                                "/ws/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api/signup/**",
@@ -47,7 +69,8 @@ public class SecurityConfig {
                                 "/api/home/**",
                                 "/api/request-board",
                                 "/api/student-board",
-                                "/api/chat/room/**",
+                                "/api/chat/**",
+                                "/api/chat/ws/**",
                                 "/api/request-board/**",
                                 "/api/student-board/**",
                                 "/api/certification/**").permitAll()
